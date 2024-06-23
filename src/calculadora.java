953ec48d12534983da067ff24efa2008d72b3be5
AndersonV123 <anderson.vilatuna@epn.edu.ec>
2024-06-23 13:17:43 -0500

    creacion de archivos

---
+++ src/calculadora.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+
+public class calculadora {
+    public JPanel mainPanel;
+    private JTree tree1;
+    public calculadora() {
+
+    }
+}
