03b6760126fec3b8fd3b36a9e6aff3addacff5ad
AndersonV123 <anderson.vilatuna@epn.edu.ec>
2024-06-28 12:35:20 -0500

    creación de la interfaz

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        JFrame frame = new JFrame(); // crea objeto LabelFrame
+        frame.setContentPane(new calculadora().mainPanel);
+        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        frame.setSize( 450, 350 );
+        frame.setVisible( true ); //
 
     }
 }
--- src/calculadora.java
+++ src/calculadora.java
@@ -1,10 +1,32 @@
 import javax.swing.*;
-import java.awt.event.ComponentAdapter;
 
 public class calculadora {
+    // Definición de componentes de la interfaz de usuario
     public JPanel mainPanel;
-    private JTree tree1;
-    public calculadora() {
+    private JTextField textoTF;
+    private JButton resetear;
+    private JButton cero;
+    private JButton uno;
+    private JButton dos;
+    private JButton tres;
+    private JButton cuatro;
+    private JButton siete;
+    private JButton ocho;
+    private JButton nueve;
+    private JButton seis;
+    private JButton punto;
+    private JButton sen;
+    private JButton cos;
+    private JButton tan;
+    private JButton raiz;
+    private JButton potencia;
+    private JButton suma;
+    private JButton multiplicacion;
+    private JButton resta;
+    private JButton divicion;
+    private JButton igual;
+    private JButton eliminar;
+    private JButton cinco;
 
-    }
+
 }
